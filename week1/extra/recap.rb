d262923716433ebbd3255232e8b2d26e8bac119b
emontesi <elliotmontesi@gmail.com>
2016-11-02 17:23:14 -0400

    JS koans

---
+++ week1/extra/recap.rb
@@ -0,0 +1,22 @@
+# def power(x,y)
+# 	puts x ** y
+# end
+
+# power(3,4)
+
+#______________________________________
+
+# def reverse(string)
+# 	reversed_string = string.reverse
+# 	puts reversed_string
+# end
+
+# reverse("this string should be reversed")
+
+#______________________________________
+
+def factorial(x)
+	
+end
+
+factorial(5)
