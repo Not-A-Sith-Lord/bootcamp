e3b7595380155dcb7d723932cb9b9914535aff19
emontesi <elliotmontesi@gmail.com>
2016-11-02 18:41:20 -0400

    js caeser it2

--- week3/day3/caeser/app.js
+++ week3/day3/caeser/app.js
@@ -5,9 +5,20 @@ function caesarCipher (message) {
 
 	letters.forEach(function(letter){
 		var char = ( letter.charCodeAt(0) );
+
+		if (char < 97) {
+			newChar = (String.fromCharCode(char));
+			newString += newChar;
+		}
+		else if (char > 123) {
+			newChar = (String.fromCharCode(char));
+			newString += newChar;
+		}
+		else {
 		var charChange = (char - 3);
 		var newChar = (String.fromCharCode(charChange));
 		newString += newChar;
+		};
 	})
 
 	return (newString);
@@ -15,7 +26,7 @@ function caesarCipher (message) {
 
 }
 
-var encrypted = caesarCipher("brutus");
+var encrypted = caesarCipher("brut$%^us");
 
 console.log(encrypted);
 
