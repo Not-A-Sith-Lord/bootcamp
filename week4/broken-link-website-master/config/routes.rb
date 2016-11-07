6bb82ea2dfc51d3515af7c249e1a6cad0d7e777e
emontesi <emontesi87@gmail.com>
2016-11-07 15:40:49 -0500

    rails practice

---
+++ week4/broken-link-website-master/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'visitors#home', as: :home
+  get '/company/about', to: 'visitors#about', as: :about
+  get '/blog', to: 'visitors#blog', as: :blog
+  get '/contact', to: 'visitors#contact', as: :contact
+
+end
