7adabf74d4210932f7279cef2b5dc998e3597111
emontesi <emontesi87@gmail.com>
2016-11-08 17:06:52 -0500

    iteration 2 in progress

---
+++ week4/address_book/app/controllers/contacts_controller.rb
@@ -0,0 +1,10 @@
+class ContactsController < ApplicationController
+
+	def index
+		@contacts = Contact.all
+		render 'index'
+	end
+
+	def new
+	end
+end
---
+++ week4/address_book/db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Contact1 = Contact.create!(name: "donald", address: "123 bs lane", phone: "555-5555", email:"contactOne@email.com")
+Contact2 = Contact.create!(name: "hillary", address: "456 bm drive", phone: "666-6666", email:"contactTwo@email.com")
+Contact3 = Contact.create!(name: "gary", address: "789 booger rd", phone: "777-7777", email:"contactThree@email.com")
+
+
