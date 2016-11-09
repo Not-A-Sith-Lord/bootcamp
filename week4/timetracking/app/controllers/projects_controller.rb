8b048aade9325d5ed748600a0ea23bec7e2b7a6b
emontesi <emontesi87@gmail.com>
2016-11-09 11:26:46 -0500

    timetracking save new project

--- week4/timetracking/app/controllers/projects_controller.rb
+++ week4/timetracking/app/controllers/projects_controller.rb
@@ -1,13 +1,26 @@
 class ProjectsController < ApplicationController
 
 	def index
-		@projects = Project.order(created_at: :desc).limit(10)
+		@my_projects = Project.order(created_at: :desc).limit(10)
 		render 'index'
 	end
 
 	def show
 		@my_project = Project.find(params[:id])
 		render :show
+	end
+
+	def new
+		@my_project = Project.new
+		render :new
+	end
+
+	def create
+		@my_project = Project.new(
+			:name => params[:project][:name],
+			:description => params[:project][:description])
+		@my_project.save
+		redirect_to project_path(@my_project)
 	end
 
 end
--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post '/calculator', to: 'calculator#process_addition'
   get '/projects', to: 'projects#index'
   # 											project_path, could be in quotes
+
+  get '/projects/new', to: 'projects#new'
   get '/projects/:id', to: 'projects#show', as: :project
+  post '/projects', to: 'projects#create'
   
 end
