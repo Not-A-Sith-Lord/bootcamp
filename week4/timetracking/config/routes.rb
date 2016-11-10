b174cc877c1afa0654e30d2672e2c63cd281584f
emontesi <emontesi87@gmail.com>
2016-11-10 10:40:17 -0500

    form validations

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get '/projects/new', to: 'projects#new'
   get '/projects/:id', to: 'projects#show', as: :project
   post '/projects', to: 'projects#create'
-  get 'projects/:project_id/time_entries', to: 'time_entries#index', as: :project_time_entries
+  get 'projects/:project_id/time_entries', to: 'time_entries#index', as: :project_time_entries_index
+  get 'projects/:project_id/time_entries/new', to: 'time_entries#new', as: :new_project_time_entry
+  post 'projects/:project_id/time_entries', to: 'time_entries#create', as: :project_time_entries
+
   
 end
