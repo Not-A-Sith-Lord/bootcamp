3ffa7680024be8a46d1ba0575115a0b117cbca01
emontesi <emontesi87@gmail.com>
2016-11-10 14:01:31 -0500

    updated time entries

--- week4/timetracking/app/controllers/time_entries_controller.rb
+++ week4/timetracking/app/controllers/time_entries_controller.rb
@@ -12,16 +12,51 @@ class TimeEntriesController < ApplicationController
 
 	def create
 		@my_project = Project.find(params[:project_id])
-		@my_entry = @my_project.time_entries.new(
-			hours: params[:time_entry][:hours],
-			minutes: params[:time_entry][:minutes],
-			date: params[:time_entry][:date],
-			comments: params[:time_entry][:comments])
+		@my_entry = @my_project.time_entries.new(entry_params)
+		
+			#------ entry params!!!
+
+			# hours: params[:time_entry][:hours],
+			# minutes: params[:time_entry][:minutes],
+			# date: params[:time_entry][:date],
+			# comments: params[:time_entry][:comments])
 		if @my_entry.save
 			redirect_to "/projects/#{@my_project.id}/time_entries"
 		else
 			render :new
 		end
 	end
+
+	def edit
+		@my_project = Project.find params[:project_id]
+		@my_entry = @my_project.time_entries.find params[:id]
+	end
+
+	def update
+
+		@my_project = Project.find params[:project_id]
+		@my_entry = @my_project.time_entries.find params[:id]
+
+		if @my_entry.update(entry_params) 
+		  redirect_to project_time_entries_path(@my_project)
+		else
+			render :edit
+		end
+	end
+
+
+	private
+
+	def entry_params
+		params.require(:time_entry).permit(:hours,:minutes,:date,:comments)
+		# hours: params[:time_entry][:hours],
+	end
+
+
+
+
+
+
+
 
 end
