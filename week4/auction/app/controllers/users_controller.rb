a3f6bf0ba24ba793253597bb17bf4e2373c85eaa
emontesi <emontesi87@gmail.com>
2016-11-10 15:51:39 -0500

    adding users

---
+++ week4/auction/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+	def index
+		@user = User.all
+		render 'index'
+	end
+
+	def new
+		@user = User.new
+		render 'users/new'
+	end
+
+	def create
+		@my_user = User.new(
+			:name => params[:user][:name],
+			:email => params[:user][:email])
+		@my_user.save
+		redirect_to user_path(@my_user)
+	end
+
+	def show
+		@my_user = User.find(params[:id])
+		render 'show'
+	end
+
+
+end
