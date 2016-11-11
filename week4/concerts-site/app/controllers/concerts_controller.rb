905a01ecc8cc3cfa1812e3f85aed0dda254f0626
emontesi <emontesi87@gmail.com>
2016-11-11 12:28:05 -0500

    concerts it1

--- week4/concerts-site/app/controllers/concerts_controller.rb
+++ week4/concerts-site/app/controllers/concerts_controller.rb
@@ -1,12 +1,30 @@
 class ConcertsController < ApplicationController
 
 	def index
-		@my_concert = Concert.all
+		@concert = Concert.all
 		render :index
 	end
 
 	def show
-		@my_concert = Concert.find(params[:id])
+		@concert = Concert.find(params[:id])
 		render :show
 	end
+
+	def new
+		@my_concert = Concert.new
+		render :new
+	end
+
+	def create
+		@my_concert = Concert.new(
+		:artist => params[:concert][:artist],
+		:venue => params[:concert][:venue],
+		:city => params[:concert][:city],
+		:date => params[:concert][:date],
+		:price => params[:concert][:price],
+		:description => params[:concert][:description])
+		@my_concert.save!
+		redirect_to concerts_path
+	end
 end
+
