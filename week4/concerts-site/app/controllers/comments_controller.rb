083c0798e96a090205cfb116ff87f3fff1c1674f
emontesi <emontesi87@gmail.com>
2016-11-12 19:19:41 -0500

    added comments

--- week4/concerts-site/app/controllers/comments_controller.rb
+++ week4/concerts-site/app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+	def new
+		@my_concert = Concert.find(params[:concert_id])
+		@my_comment = @my_concert.comment.new
+	end
+
+	def create
+		@my_concert = Concert.find(params[:concert_id])
+		@my_comment = @my_concert.comment.new(
+			:title => params[:comment][:title],
+			:content => params[:comment][:content])
+		@my_comment.save
+		redirect_to concert_path(@my_concert)
+	end
+
 end
+
