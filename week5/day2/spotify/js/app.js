686e6097d97e25b89634e6ecad4b47638b805f2d
emontesi <emontesi87@gmail.com>
2016-11-15 18:16:33 -0500

    ready for it2

--- week5/day2/spotify/js/app.js
+++ week5/day2/spotify/js/app.js
@@ -6,8 +6,7 @@ $(document).ready(function(){
 
 //=============================
 //=============================
-
-
+//=============================
 
 function searchArtist(x){
 	x.preventDefault();
@@ -17,8 +16,6 @@ function searchArtist(x){
 	var searchTerm = $(".js-search-artist").val();
 	console.log(searchTerm);
 
-	
-
 	$.ajax({
 		type: "GET",
 		url:`https://api.spotify.com/v1/search?type=artist&query=${searchTerm}`,
@@ -27,57 +24,31 @@ function searchArtist(x){
 	});
 }
 
+//=============================
+
 function showArtists(a){
 	// console.log("Success");
 	// console.log(a.artists['items'][0]);
-	var result = a.artists['items'][0];
-	console.log(result)
+	var results = a.artists['items'];
+	console.log(results)
 
+	results.forEach(function(oneResult){
 	 var artistInfo = `
-	<li> 
-	<p> ${result.name} </p>
-	<img src=" ${result.images[0].url}">
-	</li>`
+	 <ul>
+	<li> ${oneResult.name} </li>
+	<li><img src=" ${oneResult.images[0].url}"></li>
+	</ul>`
 	 $(".js-artists-list").append(artistInfo);
+	});
+
 };
 
+//=============================
 
 function handleError(errorThing){
 	console.log("Fetch Characters Error");
 	console.log(errorThing.responseText);
 }
 
+//=============================
 
-
-// function fetchCharacters(){
-// 	$.ajax({
-// 		type: "GET",
-// 		url: "https://ironhack-characters.herokuapp.com/characters",
-// 		success: showCharacters,
-// 		error: handleError,
-// 	});
-// }
-
-// function showCharacters(x) {
-// 	console.log("Success!");
-// 	console.log(x);
-
-// 	$(".js-character-list").empty();
-	
-	// x.forEach(function(oneCharacter){
-	// 	var characterItem =`
-	// 	  <li> 
-	// 	  <h3> ${oneCharacter.name} </h3>
-	// 	  <p> Weapon: ${oneCharacter.weapon} </p>
-	// 	  <p> Debt: ${oneCharacter.debt} </p>
-	// 	  <p> ID: ${oneCharacter.id} </p>
-	// 	  </li>`;
-	// 	$(".js-character-list").append(characterItem);
-	// });
-
-// }
-
-// function handleError(errorThing){
-// 	console.log("Fetch Characters Error");
-// 	console.log(errorThing.responseText);
-// }
