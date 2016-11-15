afa163c3f6f5ba7c7d2245cde5351f74659a46c5
emontesi <emontesi87@gmail.com>
2016-11-15 14:09:52 -0500

    ajax exercise

--- week5/day2/ajax/js/app.js
+++ week5/day2/ajax/js/app.js
@@ -1,11 +1,87 @@
 $(document).ready(function(){
 
 	$(".js-fetch-characters").on("click", fetchCharacters);
+	$(".js-ackbar-form").on("submit", saveAckbar);
+	$(".js-add-character-form").on("submit", saveCharacter);
+
 
 });
 
 // ========== ^^^^^^DOC READY^^^^^^^^^
 // ======================================
+
+function saveCharacter(event){
+	event.preventDefault();
+
+	var debtInput = $(".js-add-character-debt").val();
+		if (debtInput === ""){
+			debtInput = "False";
+		}
+
+	var characterInfo = {
+		name: $(".js-add-character-name").val(),
+		occupation: $(".js-add-character-occupation").val(),		
+		weapon: $(".js-add-character-weapon").val(),
+		debt: debtInput,
+	};
+
+	console.log(characterInfo);
+
+	$.ajax({
+		type: "Post",
+		url: "https://ironhack-characters.herokuapp.com/characters",
+		data: characterInfo,
+		success: showCharacter,
+		error: handleError,		
+	});
+
+}
+
+function handleError(errorThing){
+	console.log("Fetch Characters Error");
+	console.log(errorThing.responseText);
+}
+
+function showCharacter (api){
+	console.log("save character success")
+	console.log(api);
+}
+
+// ================================
+// ================================
+
+function saveAckbar (eventThing){
+	eventThing.preventDefault();
+
+	var weaponInput = $(".js-ackbar-weapon").val();
+	if(weaponInput === ""){
+		weaponInput = "Memes";
+	}
+
+
+	var ackbarInfo = {
+		name: "Admiral Ackbar",
+		occupation: "Rebel Fleet Admiral",
+		weapon: weaponInput,
+	};
+
+
+	$.ajax({
+		type: "Post",
+		url: "https://ironhack-characters.herokuapp.com/characters",
+		data: ackbarInfo,
+		success: showAckbar,
+		error: handleError,
+	});
+}
+
+function showAckbar(infoFromApi){
+	console.log("Save Ackbar success");
+	console.log(infoFromApi);
+}
+
+// ================================
+// ================================
 
 function fetchCharacters(){
 	$.ajax({
@@ -19,6 +95,8 @@ function fetchCharacters(){
 function showCharacters(x) {
 	console.log("Success!");
 	console.log(x);
+
+	$(".js-character-list").empty();
 	
 	x.forEach(function(oneCharacter){
 		var characterItem =`
@@ -33,7 +111,7 @@ function showCharacters(x) {
 
 }
 
-function handleError(errorThing){
-	console.log("Fetch Characters Error");
-	console.log(errorThing.responseText);
-}
+// function handleError(errorThing){
+// 	console.log("Fetch Characters Error");
+// 	console.log(errorThing.responseText);
+// }
