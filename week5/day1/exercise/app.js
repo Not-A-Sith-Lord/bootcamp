b19fffc7f840190ae7bd286fa7c7b63e9e9eefe4
emontesi <emontesi87@gmail.com>
2016-11-14 19:28:50 -0500

    it1

---
+++ week5/day1/exercise/app.js
@@ -0,0 +1,30 @@
+console.log("shit");
+
+
+$(document).ready(function(){
+	// ^^^ when the page is done loading
+	//  all JS EVENTS should be inside 
+
+	// $(".button").on("click", function(){
+	// 	$(".faded").append(`<img src="images/giphy-2.gif">`);
+	// });
+
+var phrases = [
+	'derp.',
+	'oi.',
+	'holy shinto'
+]
+
+
+	function randomPhrase(){
+ 	var x = phrases[Math.floor(phrases.length * Math.random())];
+		$("body").append(`${x}`);
+};
+
+randomPhrase();
+
+
+
+
+});
+
