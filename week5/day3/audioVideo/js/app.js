44a0c3f93bd659d86e9a05a4b73de8b647fe810c
emontesi <emontesi87@gmail.com>
2016-11-16 20:35:03 -0500

    it2 in progress

--- week5/day3/audioVideo/js/app.js
+++ week5/day3/audioVideo/js/app.js
@@ -1,5 +1,23 @@
 console.log("audiovideo working");
 
-$('#play').on('click', function(){
-  $('#vid1').trigger('play');
+$(document).ready(function(){
+	console.log("The page had finished loading.");
+
+	$(".js-add-video").on("click", getVideo);
+
+	function getVideo(){
+	console.log("button clicked");
+	var newVideo = prompt("What video do you want to add?");
+	var newVideoTag = `<video controls src="${newVideo}"></video>`;
+	$(".js-all-videos").append(newVideoTag);
+	};
+
+	
 });
+
+
+
+
+
+
+	 // var favImageImg = `<img src="${favImage}">`;
